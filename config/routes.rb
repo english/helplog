46aa61479c0c8dade3ef80b560a9be659ef30635
Jamie English <jamienglish@gmail.com>
2013-07-21 14:34:21 +0100

    Added convenience get route to logout

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Helplog::Application.routes.draw do
   get 'login', to: 'sessions#new', as: :login
   post 'login', to: 'sessions#create', as: :sessions
   delete 'logout', to: 'sessions#destroy', as: :logout
+  get 'logout', to: 'sessions#destroy' # convenience for logging out
 
   resources :posts do
     collection do
